427bc64c5d20c5454a55b4c1bb06826341e5c1ab
Moisés Colón Ponce <moises.colon-ponce@improving.com>
2021-11-22 17:05:41 -0600

    added mock profile pictures to mock data

--- src/Data/defaultContacts.ts
+++ src/Data/defaultContacts.ts
@@ -9,7 +9,8 @@ export const defaultContactList: ContactList = [
     emails: [
       'jennifer@jlo.com',
       'lopez2@jlo2.com',
-    ]
+    ],
+    profileImage: 'pexels-jorge-fakhouri-filho-2616958-orig (2).jpg'
   },
   {
     id: getUniqueId(),
@@ -17,7 +18,8 @@ export const defaultContactList: ContactList = [
     lastName: 'Quiñones',
     emails: [
       'denise@quinones.net',
-    ]
+    ],
+    profileImage: 'pexels-redwolf-7439890-orig (2).jpg'
   },
   {
     id: getUniqueId(),
@@ -29,7 +31,8 @@ export const defaultContactList: ContactList = [
        'joaquin3@phoenix3.xyz',
        'joaquin4@phoenix4.com',
        'joaquin5@phoenix5.mob',
-    ]
+    ],
+    profileImage: 'person-profile-pexels-jorge-fakhouri-filho-2616958.jpg'
   },
   {
     id: getUniqueId(),
@@ -39,7 +42,8 @@ export const defaultContactList: ContactList = [
        'deltoro1@Benicio1.pr',
        'deltoro2@Benicio2.co',
        'deltoro3@Benicio3.com',
-    ]
+    ],
+    profileImage: 'pexels-victor-l-2790437 (2).jpg'
   },
   {
     id: getUniqueId(),
@@ -49,7 +53,8 @@ export const defaultContactList: ContactList = [
        'dawson@rosario.com',
        'dawson2@rosario2.com',
        'dawson3@rosario3.com',
-    ]
+    ],
+    profileImage: 'pexels-marcelo-chagas-3442135 (2).jpg'
   },
   {
     id: getUniqueId(),
@@ -61,6 +66,7 @@ export const defaultContactList: ContactList = [
        'man3@luis3.com',
        'gman4@luis4.com',
        'guzman@luis5.com',
-    ]
+    ],
+    profileImage: 'pexels-lucxama-sylvain-2766224 (2).jpg'
   }
 ];
