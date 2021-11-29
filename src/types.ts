0fc2ef0d3b584510d31ebb5618125f2eb024b941
Moisés Colón Ponce <moises.colon-ponce@improving.com>
2021-11-29 12:09:45 -0600

    added URL to types

--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,30 @@
-export interface Contact {
-  id: number;
-  firstName: string;
-  lastName: string;
-  emails: string[];
-  profileImage: string;
+interface Url {
+  emailPrimary?: string
+  facebook?: string
+  instagram?: string
+  github?: string
+  linkedin?: string
+  phonePrimary?: string
+  spotify?: string
+  tiktok?: string
+  twitter?: string
+  yelp?: string
+  youtube?: string
+  website?: string
 }
 
-export type ContactList = Contact[];
+export interface Contact {
+  id: number
+  firstName: string
+  lastName: string
+  emails: string[]
+  profileImage?: string
+  url: Url
+}
+
+export type ContactList = Contact[]
 
 export interface NotificationType {
-  id: number;
-  message: string;
+  id: number
+  message: string
 }
