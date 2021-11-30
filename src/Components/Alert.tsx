b7add3ecbb4fb3dd670363b9be5b06ee0317c748
Moisés Colón Ponce <moises.colon-ponce@improving.com>
2021-11-30 16:23:01 -0600

    updated Alert component

---
+++ src/Components/Alert.tsx
@@ -0,0 +1,41 @@
+import { Fragment, FunctionComponent } from 'react'
+import Card from '../layout/CardLayout'
+import Button from '../layout/ButtonLayout'
+// import './AlertNotification.scss';
+
+interface Props {
+  visible: boolean
+  config: {
+    id?: string
+    message: string
+    onCancel: () => void
+    onConfirm: Function
+  } | null
+}
+
+const Alert: FunctionComponent<Props> = ({ visible, config }) => {
+  const onConfirmation = () => {
+    config?.onConfirm(config.id)
+    config?.onCancel()
+  }
+
+  return visible ? (
+    <div className="bg-white bg-opacity-75 fixed flex items-center justify-center left-0 top-0 w-full h-screen">
+      <Card>
+        <div className="mb-6">{config?.message}</div>
+        <div className="flex justify-center">
+          <Button isDanger={true} onClick={onConfirmation}>
+            Yes
+          </Button>
+          <Button className="ml-8" onClick={config?.onCancel}>
+            No
+          </Button>
+        </div>
+      </Card>
+    </div>
+  ) : (
+    <Fragment></Fragment>
+  )
+}
+
+export default Alert
